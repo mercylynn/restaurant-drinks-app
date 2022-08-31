2806126edde68c8202281decfc13feb3ca4557da
mercylynn <mercy.kamene@student.moringaschool.com>
2022-08-31 16:47:28 +0300

    fetch objects from server

--- src/index.js
+++ src/index.js
@@ -4,3 +4,12 @@ function getData(){
    .then((data) => populateMenu(data.menuItems))
    
 }
+function populateMenu(menuObj) {
+    let menu = document.querySelector('#menuItems')
+    menuObj.map((item) => {
+        let li = document.createElement('li')
+        li.textContent = item.name
+        menu.appendChild(li)
+        li.addEventListener('click', () => renderMenuObj(item))
+    })
+}
